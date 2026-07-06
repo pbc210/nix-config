b04b66b3d079e4a160428de3d45897e50903f5e5
PBCDev <baochaupham4096@gmail.com>
2026-07-06 13:11:41 +0700

    reformat code

--- lib/mkHome.nix
+++ lib/mkHome.nix
@@ -15,13 +15,18 @@ let
         config.allowUnfree = true;
       };
 
-      extraSpecialArgs = { inherit inputs; profile = import "${inputs.self}/profile"; };
+      extraSpecialArgs = {
+        inherit inputs;
+        profile = import "${inputs.self}/profile";
+      };
       modules = modules' ++ [ "${inputs.self}/home/desktops/${desktop}" ];
     };
 
   # For convenience, we will not add the suffix to the x86_64-linux platform.
   mk-x86_64-linux = desktop: {
-    ${desktop} = inputs.home-manager.lib.homeManagerConfiguration (mkHome { inherit desktop; });
+    ${desktop} = inputs.home-manager.lib.homeManagerConfiguration (mkHome {
+      inherit desktop;
+    });
   };
 in
 inputs.nixpkgs.lib.mergeAttrsList (map mk-x86_64-linux desktops)
