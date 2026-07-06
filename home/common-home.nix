2471997f47703026ee16d164364351d18f44b5c4
PBCDev <baochaupham4096@gmail.com>
2026-07-06 09:07:10 +0700

    setup programs and servces for home

--- home/common-home.nix
+++ home/common-home.nix
@@ -1,6 +1,11 @@
-{profile, ...}:
+{ profile, inputs, ... }:
 {
   home.username = profile.username;
   home.homeDirectory = "/home/${profile.username}";
   home.stateVersion = "26.05";
+
+  imports = [
+    "${inputs.self}/home/programs"
+#    "${inputs.self}/home/services"
+  ];
 }
