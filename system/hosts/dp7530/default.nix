a54351c1b5cfc29c68dd4a4986488d78fda6a1a0
PBCDev <baochaupham4096@gmail.com>
2026-07-13 02:48:46 +0700

    dp7530: update config bootloader

--- system/hosts/dp7530/default.nix
+++ system/hosts/dp7530/default.nix
@@ -1,11 +1,17 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   boot = {
     kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
-    loader.systemd-boot.enable = false;
+    loader.systemd-boot.enable = lib.mkForce false;
 
     lanzaboote = {
       enable = true;
       pkiBundle = "/etc/secureboot";
+      configurationLimit = 10;
+      settings = {
+        timeout = 4;
+        force-menu = false;
+        console-mode = "max";
+      };
     };
   };
 
