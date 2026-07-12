786591e0cf00e2ac77ef1bbed5c61ab025b6e185
PBCDev <baochaupham4096@gmail.com>
2026-07-12 13:45:54 +0700

    fix  spelling

--- system/services/sddm/default.nix
+++ system/services/sddm/default.nix
@@ -1,5 +1,5 @@
 { config, ... }:
 {
-  ervices.displayManager.sddm.enable = config.host-config.useDesktop;
+  services.displayManager.sddm.enable = config.host-config.useDesktop;
   services.displayManager.sddm.wayland.enable = true;
 }
