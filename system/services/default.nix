92af530d28c02ad1517790ec3b5d5f5d947a3b38
PBCDev <baochaupham4096@gmail.com>
2026-07-13 17:01:19 +0700

    vaultwarden: install and config

--- system/services/default.nix
+++ system/services/default.nix
@@ -10,7 +10,7 @@
     ./pipewire
     ./sddm
     ./systemd-resolved
-    # ./vaultwarden
+    ./vaultwarden
     ./xserver
   ];
 }
---
+++ system/services/vaultwarden/default.nix
@@ -0,0 +1,23 @@
+{
+  services.vaultwarden = {
+    enable = true;
+
+    config = {
+      ROCKET_ADDRESS = "127.0.0.1";
+      ROCKET_PORT = 8222;
+
+      DOMAIN = "https://bitwarden.localhost";
+    };
+  };
+
+  services.nginx.virtualHosts."bitwarden.localhost" = {
+    forceSSL = true;
+    sslCertificate = "/var/lib/nginx/bitwarden.crt";
+    sslCertificateKey = "/var/lib/nginx/bitwarden.key";
+
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:8222";
+      proxyWebsockets = true;
+    };
+  };
+}
