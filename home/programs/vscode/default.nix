effd43e283ea583e868adb7996b6c621850b33a2
PBCDev <baochaupham4096@gmail.com>
2026-07-13 13:12:14 +0700

    fmt

--- home/programs/vscode/default.nix
+++ home/programs/vscode/default.nix
@@ -2,28 +2,28 @@
 {
   programs.vscode = {
     enable = true;
-  profiles.${profile.userName} = {
-    extensions = with inputs.vscode-extensions.extensions.${pkgs.system}.vscode-marketplace; [
-      diegoulloao.neofusion-theme
-      pkief.material-icon-theme
+    profiles.${profile.userName} = {
+      extensions = with inputs.vscode-extensions.extensions.${pkgs.system}.vscode-marketplace; [
+        diegoulloao.neofusion-theme
+        pkief.material-icon-theme
 
-      # nix
-      arrterian.nix-env-selector
-      jnoortheen.nix-ide
-      brettm12345.nixfmt-vscode
-      editorconfig.editorconfig
-    ];
+        # nix
+        arrterian.nix-env-selector
+        jnoortheen.nix-ide
+        brettm12345.nixfmt-vscode
+        editorconfig.editorconfig
+      ];
 
-    userSettings = {
-      "workbench.activityBar.compact" = true;
-      "workbench.sideBar.location" = "right";
-      "workbench.activityBar.location" = "bottom";
-      "workbench.colorTheme" = "Neofusion Theme";
-      "workbench.iconTheme" = "material-icon-theme";
+      userSettings = {
+        "workbench.activityBar.compact" = true;
+        "workbench.sideBar.location" = "right";
+        "workbench.activityBar.location" = "bottom";
+        "workbench.colorTheme" = "Neofusion Theme";
+        "workbench.iconTheme" = "material-icon-theme";
 
-      "files.autoSave" = "afterDelay";
-      "git.autofetch" = true;
+        "files.autoSave" = "afterDelay";
+        "git.autofetch" = true;
+      };
     };
-  };
   };
 }
