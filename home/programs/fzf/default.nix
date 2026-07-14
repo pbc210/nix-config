82bc9347d0e032b230f37b4c7583de13cf80f84a
PBCDev <baochaupham4096@gmail.com>
2026-07-15 01:36:35 +0700

    fzf: using fd

--- home/programs/fzf/default.nix
+++ home/programs/fzf/default.nix
@@ -1,5 +1,8 @@
 {
   programs.fzf = {
     enable = true;
+
+    defaultCommand = "fd --type f --strip-cwd-prefix --hidden";
+    fileWidgetCommand = "fd --type f --strip-cwd-prefix --hidden";
   };
 }
