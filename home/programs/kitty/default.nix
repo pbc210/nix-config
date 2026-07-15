972fe89ad8d071057779d897652f9a105874fcc5
PBCDev <baochaupham4096@gmail.com>
2026-07-15 10:31:58 +0700

    kitty: setup keybind

--- home/programs/kitty/default.nix
+++ home/programs/kitty/default.nix
@@ -65,5 +65,23 @@
       color7 = "#e0d9c7";
       color15 = "#e0d9c7";
     };
+    keybindings = {
+      "alt+a" = "new_tab";
+      "alt+s" = "close_tab";
+
+      "alt+f" = "previous_tab";
+      "alt+d" = "next_tab";
+
+      "alt+1" = "goto_tab 1";
+      "alt+2" = "goto_tab 2";
+      "alt+3" = "goto_tab 3";
+      "alt+4" = "goto_tab 4";
+      "alt+5" = "goto_tab 5";
+      "alt+6" = "goto_tab 6";
+      "alt+7" = "goto_tab 7";
+      "alt+8" = "goto_tab 8";
+      "alt+9" = "goto_tab 9";
+      "alt+10" = "goto_tab 10";
+    };
   };
 }
