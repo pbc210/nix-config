69caefe03578c31e921623c44c45535e3d6e06e9
PBCDev <baochaupham4096@gmail.com>
2026-07-16 22:53:15 +0700

    nvim(ui): config status-bar using lualine

---
+++ home/programs/nvim/ui/status-bar/default.nix
@@ -0,0 +1,46 @@
+{
+  programs.nixvim.plugins = {
+    web-devicons.enable = true;
+
+    lualine = {
+      enable = true;
+
+      settings = {
+        options = {
+          theme = "auto";
+
+          componentSeparators = { left = "|"; right = "|"; };
+          sectionSeparators = { left = ""; right = ""; };
+
+          globalstatus = true;
+        };
+
+        lualine_a = [ "mode" ];
+
+        lualine_b = [ "branch" "diff" ];
+
+        lualine_c = [
+          {
+            __unkeyed-1 = "filename";
+            file_status = true;
+            path = 1;
+          }
+        ];
+
+        lualine_x = [
+          {
+            __unkeyed-1 = "diagnostics";
+            sources = [ "nvim_diagnostic" ];
+            symbols = { error = " "; warn = " "; info = "󰋼 "; hint = " "; };
+          }
+            "encoding"
+            "fileformat"
+            "filetype"
+          ];
+
+        lualine_y = [ "progress" ];
+        lualine_z = [ "location" ];
+      };
+    };
+  };
+}
