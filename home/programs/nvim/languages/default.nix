fd5f5a988b6a71c103e18996ff0e8a29127ad055
PBCDev <baochaupham4096@gmail.com>
2026-07-18 13:38:32 +0700

    nvim(lang): config treesitter

--- home/programs/nvim/languages/default.nix
+++ home/programs/nvim/languages/default.nix
@@ -1,21 +1,45 @@
 {
-  programs.nixvim.plugins.lsp = {
-    enable = true;
+  programs.nixvim.plugins = {
+    lsp = {
+      enable = true;
 
-    keymaps = {
-      lspBuf = {
-        "gd" = "definition";
-        "gD" = "declaration";
-        "gi" = "implementation";
-        "gt" = "type_definition";
-        "K"  = "hover";
-        "<F2>" = "rename";
-        "<F4>" = "code_action";
+      keymaps = {
+        lspBuf = {
+          "gd" = "definition";
+          "gD" = "declaration";
+          "gi" = "implementation";
+          "gt" = "type_definition";
+          "K"  = "hover";
+          "<F2>" = "rename";
+          "<F4>" = "code_action";
+        };
+
+        diagnostic = {
+          "[d" = "goto_prev";
+          "]d" = "goto_next";
+        };
       };
+    };
+    treesitter = {
+      enable = true;
 
-      diagnostic = {
-        "[d" = "goto_prev";
-        "]d" = "goto_next";
+      settings = {
+        highlight = {
+          enable = true;
+          additional_vim_regex_highlighting = false;
+        };
+
+        indent.enable = true;
+
+        incremental_selection = {
+          enable = true;
+          keymaps = {
+            init_selection = ",";
+            node_incremental = ",";
+            node_decremental = "<BS>";
+            scope_incremental = "<gCR>";
+          };
+        };
       };
     };
   };
--- home/programs/nvim/languages/nix/default.nix
+++ home/programs/nvim/languages/nix/default.nix
@@ -1,3 +1,4 @@
+{pkgs, ...}:
 {
   programs.nixvim.plugins.lsp.servers.nixd = {
     enable = true;
@@ -11,4 +12,6 @@
       };
     };
   };
+
+  programs.nixvim.plugins.treesitter.grammarPackages = [ pkgs.vimPlugins.nvim-treesitter.builtGrammars.nix ];
 }
