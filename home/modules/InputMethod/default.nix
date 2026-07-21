c71b099d4aa7c687da8aceaa465006905f88adb2
PBCDev <baochaupham4096@gmail.com>
2026-07-21 20:23:44 +0700

    fcitx5: fix link config file

--- home/modules/InputMethod/default.nix
+++ home/modules/InputMethod/default.nix
@@ -11,7 +11,7 @@
     fcitx5.waylandFrontend = true;
   };
 
-  xdg.configFile."fcitx5/config".source = config.lib.file.mkOutOfStoreSymlink ./fcitx5.conf;
+  xdg.configFile."fcitx5/config".source = config.lib.file.mkOutOfStoreSymlink ./fcitx5/.conf;
   xdg.configFile."fcitx5/conf/clipboard.conf".source = config.lib.file.mkOutOfStoreSymlink ./fcitx5/clipboard.conf;
   xdg.configFile."fcitx5/conf/notifications.conf".source = config.lib.file.mkOutOfStoreSymlink ./fcitx5/notifications.conf;
   xdg.configFile."fcitx5/profile".source = config.lib.file.mkOutOfStoreSymlink ./fcitx5/profile;
