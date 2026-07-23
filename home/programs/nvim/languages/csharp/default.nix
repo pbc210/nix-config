64623313931ddcd0e54e6eb6d74bb1b0e2eb80a1
PBCDev <baochaupham4096@gmail.com>
2026-07-23 09:49:19 +0700

    nvim(lang/cs): use package packages.roslyn-ls for roslyn_ls lsp

--- home/programs/nvim/languages/csharp/default.nix
+++ home/programs/nvim/languages/csharp/default.nix
@@ -1,6 +1,9 @@
+{ pkgs, ... }:
 {
   programs.nixvim.plugins.lsp.servers.roslyn_ls = {
     enable = true;
+
+    package = pkgs.roslyn-ls;
   };
 
   programs.nixvim.plugins.conform-nvim.settings.formatters_by_ft = { cs = "csharpier"; };
