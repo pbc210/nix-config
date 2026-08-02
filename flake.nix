cd797972aebdc13e35dde2102dbd2ac5f1fc9c8a
PBCDev <baochaupham4096@gmail.com>
2026-08-02 17:14:49 +0700

    cache: setup personal binary cache

--- flake.nix
+++ flake.nix
@@ -3,11 +3,13 @@
 
   nixConfig = {
     extra-substituters = [
+      "https://pbcdev.cachix.org"
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
     ];
 
     extra-trusted-public-keys = [
+      "pbcdev.cachix.org-1:iZbrMY/10HM5BQPXeIIHkGoDc4boLuSZYiZuPhIn9P8="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
