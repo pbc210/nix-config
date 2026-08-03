0a2168d7254e4035c650afd24a64624d9eefa575
PBCDev <baochaupham4096@gmail.com>
2026-08-03 16:50:24 +0700

    nvim(file/auto-save): remove execution_message

--- home/programs/nvim/file/auto-save.nix
+++ home/programs/nvim/file/auto-save.nix
@@ -4,10 +4,7 @@
 
     settings = {
       enabled = true;
-      execution_message = {
-        message.__raw = ''"AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S")'';
-        cleaning_interval = 1250;
-      };
+
       trigger_events = {
         immediate_save = [ "BufLeave" "FocusLost" ];
         defer_save = [ "InsertLeave" "TextChanged" ];
