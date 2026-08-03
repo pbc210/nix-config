ca5c9b15ec64eac37827cb9e153cc30efaa24a63
PBCDev <baochaupham4096@gmail.com>
2026-08-03 11:06:46 +0700

    nvim(git): using current_line_blame_opts

--- home/programs/nvim/git/gitsigns.nix
+++ home/programs/nvim/git/gitsigns.nix
@@ -2,6 +2,15 @@
   programs.nixvim.plugins.gitsigns = {
     enable = true;
     settings = {
+      current_line_blame = true;
+
+
+      current_line_blame_opts = {
+        virt_text = true;
+        virt_text_pos = "eol";
+        delay = 500;
+      };
+
       signs = {
         add = { text = "┃"; };
         change = { text = "┃"; };
