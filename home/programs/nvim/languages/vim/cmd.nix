8f2c4e65cb8dc5f9a00e19001261533e89eb9be3
PBCDev <baochaupham4096@gmail.com>
2026-08-04 04:02:27 +0700

    nvim(lang/vim): use blink.cmp for cmd

--- home/programs/nvim/languages/vim/cmd.nix
+++ home/programs/nvim/languages/vim/cmd.nix
@@ -22,4 +22,36 @@
       };
     };
   };
+
+  programs.nixvim.plugins.blink-cmp.settings = {
+    cmdline = {
+      enabled = true;
+
+      keymap = {
+        preset = "cmdline";
+        # "<Tab>" = [ "show" "accept" ];
+        # "<C-n>" = [ "select_next" ];
+        # "<C-p>" = [ "select_prev" ];
+      };
+
+      completion = {
+        menu.auto_show = true;
+
+        list.selection.preselect = false;
+      };
+
+      sources.__raw = ''
+        function()
+          local type = vim.fn.getcmdtype()
+          if type == "/" or type == "?" then
+            return { "buffer" }
+          end
+          if type == ":" then
+            return { "cmdline" }
+          end
+          return {}
+        end
+      '';
+    };
+  };
 }
