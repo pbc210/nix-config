fefe1734d5195b33454256c911ee90f30686efb7
PBCDev <baochaupham4096@gmail.com>
2026-08-06 00:04:18 +0700

    zsh: resetup keymaps

--- home/programs/zsh/init.zsh
+++ home/programs/zsh/init.zsh
@@ -2,5 +2,6 @@ bindkey '^H' backward-kill-word
 bindkey '^[[1;5D' backward-word
 bindkey '^[[1;5C' forward-word
 
-bindkey '^@' autosuggest-accept
-bindkey '\e\t' _atuin_search_widget
+bindkey '\t' autosuggest-accept
+bindkey '^@' _atuin_search_widget
+
